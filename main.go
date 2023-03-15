8232d9837f8c4ee9397d72b8c471f7afc49574a6
langming(明朗) <langming@upchina.com>
2023-03-15 19:36:18 +0800

    go启动

--- main.go
+++ main.go
@@ -9,14 +9,15 @@ import (
 )
 
 func main() {
-	// Get server config
-	cfg := taf.GetServerConfig()
 
 	// 启动boot
 	boot.Boot("")
 
 	// New servant imp
 	{
+		// Get server config
+		cfg := taf.GetServerConfig()
+
 		imp := new(MingHelloImp)
 		err := imp.Init()
 		if err != nil {
@@ -28,20 +29,20 @@ func main() {
 	}
 
 	// 支持HTTP
-	{
-		//mux := &taf.TafHttpMux{}
-		//mux.HandleFunc("/ming", func(w http.ResponseWriter, r *http.Request) {
-		//	whereMaps := map[string]string{
-		//		//"nickname": "张三",
-		//		"order": "createtime ASC",
-		//	}
-		//	_, teachers, _ := teacher_repository.NewTeacher().QueryTeachers(0, 100, whereMaps)
-		//	resp, _ := json.Marshal(teachers)
-		//
-		//	_, _ = w.Write(resp)
-		//})
-		//taf.AddHttpServant(mux, cfg.App+"."+cfg.Server+".MingHelloObj") //Register http server
-	}
+	//{
+	//	mux := &taf.TafHttpMux{}
+	//	mux.HandleFunc("/ming", func(w http.ResponseWriter, r *http.Request) {
+	//		whereMaps := map[string]string{
+	//			//"nickname": "张三",
+	//			"order": "createtime ASC",
+	//		}
+	//		_, teachers, _ := teacher_repository.NewTeacher().QueryTeachers(0, 100, whereMaps)
+	//		resp, _ := json.Marshal(teachers)
+	//
+	//		_, _ = w.Write(resp)
+	//	})
+	//	taf.AddHttpServant(mux, cfg.App+"."+cfg.Server+".MingHelloObj") //Register http server
+	//}
 
 	// Run application
 	taf.Run()
